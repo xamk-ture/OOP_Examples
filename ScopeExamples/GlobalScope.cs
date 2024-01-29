8ba454a98760c46cff92fe87c1514d168a43aad2
Tuomas Reijonen <hture01@xamk.fi>
2024-01-29 14:05:33 +0200

    Added scope examples

---
+++ ScopeExamples/GlobalScope.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScopeExamples
+{
+    /// <summary>
+    /// This class can be accessed from anywhere
+    /// </summary>
+    public class GlobalScope
+    {
+        /// <summary>
+        /// This property can be accessed outside of this class
+        /// </summary>
+        public string GlobalString = "I am a global string";
+
+        /// <summary>
+        /// This cannot be accessed outside of this class
+        /// </summary>
+        private string privateString = "I am a private string";
+
+    }
+
+    /// <summary>
+    /// This class can only be accessed from within this assembly
+    /// </summary>
+    protected class InternalClass
+    {
+        public string InternalString = "I am a global string";
+    }
+
+    public static class Tester
+    {
+        public static void Test()
+        {
+            GlobalScope globalScope = new();
+            Console.WriteLine(globalScope.GlobalString);
+
+            InternalClass internalClass = new();
+            Console.WriteLine(internalClass.InternalString);
+        }
+    }
+}
---
+++ ScopeExamples/Program.cs
@@ -0,0 +1,48 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ScopeExamples
+{
+    internal class Program
+    {
+        string globalString = "I am a global string";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public void LocalScopeExample()
+        {
+            int localNumber = 5;
+            Console.WriteLine(localNumber);
+
+            for (int number = 0; number < 10; number++)
+            {
+                Console.WriteLine(number);
+            }
+
+            //This will not work because number is not in scope and cannot be accessed
+            //Console.WriteLine(number);
+        }
+
+        public void ScopeTests()
+        {
+            //This will not work because localNumber is not in scope and cannot be accessed
+            //Console.WriteLine(localNumber);
+
+            //This will work because globalString is in scope and can be accessed
+            Console.WriteLine(globalString);
+
+            //We can create a new instance of GlobalScope because it's scope is global/public
+            GlobalScope globalScope = new();
+
+            Console.WriteLine(globalScope.GlobalString);
+
+            //This will not work because privateString is not in scope and cannot be accessed
+            //Console.WriteLine(globalScope.privateString);
+        }
+
+
+
+    }
+}
