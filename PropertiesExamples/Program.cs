0193f05a266b7a8e2257f1eb0f1d44ed2dc814c0
Tuomas Reijonen <hture01@xamk.fi>
2024-02-07 13:44:11 +0200

    Added properties examples

---
+++ PropertiesExamples/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace PropertiesExamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            User user = new User();
+
+            //Using properties to set and get the user's name
+            user.Name = "Bob";
+            Console.WriteLine(user.Name);
+
+            //Using properties with a private backing field
+            user.Name2 = "Alice";
+            Console.WriteLine(user.Name2);
+
+            //Using methods to set and get the user's name
+            user.SetName("Charlie");
+
+            //Using methods to set and get the user's name
+            Console.WriteLine(user.GetName());
+        }
+    }
+}
---
+++ PropertiesExamples/User.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesExamples
+{
+    public class User
+    {
+        /// <summary>
+        /// Property for the user's name
+        /// </summary>
+        public string Name { get; set; }
+
+        private string privateName;
+
+        /// <summary>
+        /// Example of a property with a private backing field. 
+        /// This is what the compiler does for you when you use the shorthand property syntax
+        /// </summary>
+        public string Name2
+        {
+            get
+            {
+                return privateName;
+            }
+            set
+            { 
+                privateName = value; 
+            }
+        }
+
+        /// <summary>
+        /// Full method for getting the user's name, that get represents
+        /// </summary>
+        /// <returns></returns>
+        public string GetName()
+        {
+            return privateName;
+        }
+
+        /// <summary>
+        /// Full method for setting the user's name, that set represents
+        /// </summary>
+        /// <param name="name"></param>
+        public void SetName(string name)
+        {
+            privateName = name;
+        }
+
+    }
+}
